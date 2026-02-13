58984b23b2e30a13093719f75f7df4ab36e93ac1
Emmanuel Leroy <emmanuel.leroy@oracle.com>
2026-02-13 11:34:23 -0800

    fix NSG =true case

--- add_on_istio.tf
+++ add_on_istio.tf
@@ -23,38 +23,3 @@ module "istio_gateway_crds" {
     module.istio_deployment_using_addon_manager
   ]
 }
-
-# module "istio_workload_identity_policy" {
-#   source               = "./modules/iam/workload_identity"
-#   compartment_id       = var.cluster_compartment_id
-#   workload_name        = "istio"
-#   service_account_name = "istio"
-#   namespace            = "istio-system"
-#   permissions = [
-#     "manage load-balancers",
-#     "use virtual-network-family",
-#     "manage cabundles",
-#     "manage cabundle-associations",
-#     "manage leaf-certificates",
-#     "read leaf-certificate-bundles",
-#     "manage leaf-certificate-versions",
-#     "manage certificate-associations",
-#     "read certificate-authorities",
-#     "manage certificate-authority-associations",
-#     "read certificate-authority-bundles",
-#     "read public-ips",
-#     "manage floating-ips",
-#     "manage waf-family",
-#     "read cluster-family",
-#     "use tag-namespaces"
-#   ]
-#   defined_tags = var.defined_tags
-#   cluster_id   = oci_containerengine_cluster.oci_oke_cluster.id
-#   providers = {
-#     oci = oci.home_region
-#   }
-#   depends_on = [
-#     module.istio_deployment_using_addon_manager, # metrics server depends on cert-manager
-#     oci_containerengine_node_pool.oci_oke_node_pool
-#   ]
-# }
