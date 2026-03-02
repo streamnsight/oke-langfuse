0fb0f8249b20f13eb3c9e204ac473ac7ee1b3c27
Emmanuel Leroy <emmanuel.leroy@oracle.com>
2026-03-02 15:28:20 -0800

    cleanup

--- app_common.tf
+++ app_common.tf
@@ -56,7 +56,6 @@ module "builder_instance" {
   compartment_id      = var.cluster_compartment_id
   availability_domain = local.ADs[0]
   subnet_id           = var.use_existing_vcn ? local.node_pools[0]["subnet"] : oci_core_subnet.oke_nodepool_subnet[0].id
-  # subnet_id           = var.use_existing_vcn ? var.public_lb_subnet : oci_core_subnet.oke_lb_subnet[0].id
   display_name  = "${local.cluster_name_sanitized}-builder"
   compute_shape = local.node_pools[0].node_shape
   image_id      = local.node_pools[0].image_id
